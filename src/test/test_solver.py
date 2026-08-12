9ed1c104abdc00499069c30e3ce493e19111e2a3
hbandura <hbandura@gmail.com>
2026-08-12 13:53:08 -0300

    Fix test folder structure

--- src/test/test_solver.py
+++ src/test/test_solver.py
@@ -1,6 +1,6 @@
 import unittest
-import fbsm
-import numpy as np
+from fbsm.solver import *
+
 
 class ControlTest(unittest.TestCase):
     def test_set_values(self):
@@ -9,7 +9,7 @@ class ControlTest(unittest.TestCase):
         t_min_max = [-1, 7]
         ini = [1, -3, 7]
         t_values = [-1, 2, 3, 7]
-        control = fbsm.Control(_f, t_min_max, ini, t_values)
+        control = Control(_f, t_min_max, ini, t_values)
         self.assertEqual(control.eqs, _f)
         self.assertEqual(control.t_span, t_min_max)
         self.assertEqual(control.x0, ini)
@@ -19,23 +19,23 @@ class ControlTest(unittest.TestCase):
 class StrictIncrDecrTest(unittest.TestCase):
     def test_increasing_values(self):
         v = [-12, 0, 3, 4, 7, 9, 15]
-        self.assertTrue(fbsm.strict_incr(v))
-        self.assertFalse(fbsm.strict_decr(v))
+        self.assertTrue(strict_incr(v))
+        self.assertFalse(strict_decr(v))
 
     def test_decreasing_values(self):
         v = [37, 32, 10, 8, -2, -4]
-        self.assertFalse(fbsm.strict_incr(v))
-        self.assertTrue(fbsm.strict_decr(v))
+        self.assertFalse(strict_incr(v))
+        self.assertTrue(strict_decr(v))
 
     def test_non_strict(self):
         v = [2, 2]
-        self.assertFalse(fbsm.strict_incr(v))
-        self.assertFalse(fbsm.strict_decr(v))
+        self.assertFalse(strict_incr(v))
+        self.assertFalse(strict_decr(v))
 
     def test_empty(self):
         v = []
-        self.assertTrue(fbsm.strict_incr(v))
-        self.assertTrue(fbsm.strict_decr(v))
+        self.assertTrue(strict_incr(v))
+        self.assertTrue(strict_decr(v))
 
 
 class FlipAllTest(unittest.TestCase):
@@ -50,13 +50,13 @@ class FlipAllTest(unittest.TestCase):
             np.array([5, 6, 7]),
             np.array([2, -1, 0])
         ]
-        actual = fbsm.flip_all(arrs)
+        actual = flip_all(arrs)
         self.assertEqual(len(expected), len(actual))
         for i in range(len(expected)):
             self.assertTrue((expected[i] == actual[i]).all())
 
     def test_flip_empty(self):
-        self.assertEqual([], fbsm.flip_all([]))
+        self.assertEqual([], flip_all([]))
 
 
 class WithArrEvTest(unittest.TestCase):
@@ -74,7 +74,7 @@ class WithArrEvTest(unittest.TestCase):
             pass
         except TypeError as e:
             pass
-        f2 = fbsm.with_array_evaluation(f)
+        f2 = with_array_evaluation(f)
         return f2(x)
 
     def test_with_numpy(self):
